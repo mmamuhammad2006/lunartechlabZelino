b5a6f1d85ec168394a318ccfeafeeab34015d71b
mmamuhammad2006 <168419071+mmamuhammad2006@users.noreply.github.com>
2024-10-21 22:18:30 +0500

    Add more caching for good performace

--- server/api/index.js
+++ server/api/index.js
@@ -11,7 +11,17 @@ app.use(compression());
 helmetMiddleware(app);
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../../public'),{ /// caching statci files
+    maxAge: '1d',
+    etag: true,
+    lastModified: true,
+    setHeaders: (res,path) => {
+        if(path.endsWith('.js') || path.endsWith('.css') || path.endsWith('.jpg') ||
+         path.endsWith('.jpeg') || path.endsWith('.png') || path.endsWith('.mp4')){
+            res.set('Cache-Control','public, max-age=86400');
+        }
+    }
+}));
 app.use(expressLayouts);
 
 app.set('views', path.join(__dirname, '../../views'));
@@ -35,6 +45,6 @@ app.use('/',whatsappRoute);
 const sendMail = require('../controller/sendMailController');
 app.use('/',sendMail);
 
-// const port = process.env.PORT || 5000;
-// app.listen(port);
+const port = process.env.PORT || 5000;
+app.listen(port);
 module.exports = app;
