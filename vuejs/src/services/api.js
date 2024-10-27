5d5ffea2bcf0155f894304e4c9236496357620f7
mmamuhammad2006 <168419071+mmamuhammad2006@users.noreply.github.com>
2024-10-27 23:20:31 +0500

    update vercel.json and vite.config.json

--- vuejs/src/services/api.js
+++ vuejs/src/services/api.js
@@ -1,11 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 // register  a user
 export const sendEmail = async (formData) => {
-    try{
-        const response = await axios.post('http://localhost:5000/send-mail',formData);
-        return response.data;
-    }catch(err){
-        throw err;
-    }   
-}
+  try {
+    const response = await axios.post(
+      "http://localhost:5000/api/send-mail",
+      formData
+    );
+    return response.data;
+  } catch (err) {
+    throw err;
+  }
+};
