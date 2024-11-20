bc869d5c93d35b579c9fc04acb8a7d4779a7ab4f
mmamuhammad2006 <168419071+mmamuhammad2006@users.noreply.github.com>
2024-11-20 19:59:16 +0500

    add web domain in vuejs api.js

--- vuejs/src/services/api.js
+++ vuejs/src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export const sendEmail = async (formData) => {
   try {
     const response = await axios.post(
-      "http://localhost:5000/api/send-mail",
+      "https://www.lunartechlab.com/api/send-mail",
       formData
     );
     return response.data;
